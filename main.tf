fcf14fee9e60da88f175b6b5b29e2de8e5a4a629
Ekemini Udoh <ekeminiudoh@Ekeminis-MacBook-Pro-2.local>
2022-10-30 23:13:50 -0700

    Add Service User, Schema, Grants

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "snowflake" {
-  role  = "SYSADMIN"
-  region   = "US-EAST-2.aws"
+  role   = "SYSADMIN"
+  region = "US-EAST-2.aws"
 }
 
 resource "snowflake_database" "db" {
-  name     = "TF_DEMO"
+  name = "TF_DEMO"
 }
 
 resource "snowflake_warehouse" "warehouse" {
@@ -23,62 +23,57 @@ resource "snowflake_warehouse" "warehouse" {
   auto_suspend = 60
 }
 
-    provider "snowflake" {
-        alias = "security_admin"
-        role  = "SECURITYADMIN"
-    }
-    resource "snowflake_role" "role" {
-        provider = snowflake.security_admin
-        name     = "TF_DEMO_SVC_ROLE"
-    }
-    resource "snowflake_database_grant" "grant" {
-        provider          = snowflake.security_admin
-        database_name     = snowflake_database.db.name
-        privilege         = "USAGE"
-        roles             = [snowflake_role.role.name]
-        with_grant_option = false
-    }
-    resource "snowflake_schema" "schema" {
-        database   = snowflake_database.db.name
-        name       = "TF_DEMO"
-        is_managed = false
-    }
-    resource "snowflake_schema_grant" "grant" {
-        provider          = snowflake.security_admin
-        database_name     = snowflake_database.db.name
-        schema_name       = snowflake_schema.schema.name
-        privilege         = "USAGE"
-        roles             = [snowflake_role.role.name]
-        with_grant_option = false
-    }
-    resource "snowflake_warehouse_grant" "grant" {
-        provider          = snowflake.security_admin
-        warehouse_name    = snowflake_warehouse.warehouse.name
-        privilege         = "USAGE"
-        roles             = [snowflake_role.role.name]
-        with_grant_option = false
-    }
-    resource "tls_private_key" "svc_key" {
-        algorithm = "RSA"
-        rsa_bits  = 2048
-    }
-    resource "snowflake_user" "user" {
-        provider          = snowflake.security_admin
-        name              = "tf_demo_user"
-        default_warehouse = snowflake_warehouse.warehouse.name
-        default_role      = snowflake_role.role.name
-        default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-        rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-    }
-    resource "snowflake_role_grants" "grants" {
-        provider  = snowflake.security_admin
-        role_name = snowflake_role.role.name
-        users     = [snowflake_user.user.name]
-    }
-
-output "snowflake_svc_public_key" {
-    value = tls_private_key.svc_key.public_key_pem
+provider "snowflake" {
+  alias  = "security_admin"
+  role   = "SECURITYADMIN"
+  region = "US-EAST-2.aws"
 }
-output "snowflake_svc_private_key" {
-    value = tls_private_key.svc_key.private_key_pem
+
+resource "snowflake_role" "role" {
+  provider = snowflake.security_admin
+  name     = "TF_DEMO_SVC_ROLE"
+}
+resource "snowflake_database_grant" "grant" {
+  provider          = snowflake.security_admin
+  database_name     = snowflake_database.db.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+resource "snowflake_schema" "schema" {
+  database   = snowflake_database.db.name
+  name       = "TF_DEMO"
+  is_managed = false
+}
+resource "snowflake_schema_grant" "grant" {
+  provider          = snowflake.security_admin
+  database_name     = snowflake_database.db.name
+  schema_name       = snowflake_schema.schema.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+resource "snowflake_warehouse_grant" "grant" {
+  provider          = snowflake.security_admin
+  warehouse_name    = snowflake_warehouse.warehouse.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+resource "tls_private_key" "svc_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+resource "snowflake_user" "user" {
+  provider          = snowflake.security_admin
+  name              = "tf_demo_user"
+  default_warehouse = snowflake_warehouse.warehouse.name
+  default_role      = snowflake_role.role.name
+  default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
+  rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
+}
+resource "snowflake_role_grants" "grants" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_role.role.name
+  users     = [snowflake_user.user.name]
 }
